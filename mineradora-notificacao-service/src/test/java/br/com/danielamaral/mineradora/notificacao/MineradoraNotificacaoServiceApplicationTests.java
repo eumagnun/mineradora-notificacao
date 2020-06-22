a68f2319cedbe35fc17d6c10614ad83af612d900
Daniel Amaral <eumagnun@gmail.com>
2020-06-22 20:33:51 -0300

    first commit

---
+++ mineradora-notificacao-service/src/test/java/br/com/danielamaral/mineradora/notificacao/MineradoraNotificacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielamaral.mineradora.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineradoraNotificacaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
